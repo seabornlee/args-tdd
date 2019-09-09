aade25c2eddfcaf98c6b15bd3b86f4c16a58095d
Seaborn Lee <seabornlee.cn@gmail.com>
2019-09-09 17:14:30 +0800

    Revert "Refactoring to pattern"
    This reverts commit 80237c8fa5856392e1741765ad112975c83938ee.

--- src/main/java/ArgsParser.java
+++ src/main/java/ArgsParser.java
@@ -1,24 +1,50 @@
-import flags.Flag;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.util.Optional;
+import static java.lang.Integer.parseInt;
+import static java.util.Arrays.asList;
 
 public class ArgsParser {
 
-    private final Schema schema = new Schema();
+    private Map<String, Object> values;
+    private Map<String, String> schemaMap;
 
-    public ArgsParser(String schemaAsString) {
-        schema.parse(schemaAsString);
+    public ArgsParser(String schema) {
+        schemaMap = parseSchema(schema);
     }
 
     public void parse(String[] args) {
-        schema.getFlags().forEach(flag -> flag.parseValue(args));
+        values = new HashMap<>();
+
+        for (Map.Entry<String, String> entry : schemaMap.entrySet()) {
+            String flagName = entry.getKey();
+            String flagType = entry.getValue();
+
+            if (flagType.equals("bool")) {
+                values.put(flagName, asList(args).contains("-" + flagName));
+            } else {
+                String value = args[asList(args).indexOf("-" + flagName) + 1];
+                if (flagType.equals("int")) {
+                    values.put(flagName, parseInt(value));
+                } else {
+                    values.put(flagName, value);
+                }
+            }
+        }
     }
 
-    public Object getValue(String flagName) {
-        Optional<Flag> flag = schema.getFlags()
-                .stream()
-                .filter((f) -> f.getName().equals(flagName))
-                .findFirst();
-        return flag.get().getValue();
+    private Map<String, String> parseSchema(String schema) {
+        Map<String, String> schemaMap = new HashMap<>();
+        String[] array = schema.split(" ");
+        for (String pair : array) {
+            String[] flagDefinition = pair.split(":");
+            schemaMap.put(flagDefinition[0], flagDefinition[1]);
+        }
+        return schemaMap;
+    }
+
+    public Object getValue(String flag) {
+        return values.get(flag);
     }
 }
